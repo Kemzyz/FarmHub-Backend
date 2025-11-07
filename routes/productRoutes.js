976e874e81d8cd68d331ee24e24d19e0ac6b526d
Ehikwe Nkemkamma <ehikwenkemkamma@gmail.com>
2025-11-07 17:49:32 +0100

    Updated Version

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getProducts, getProductById } = require('../controllers/productController');
+const { createProduct, getProducts, getProductById, updateProduct, deleteProduct} = require('../controllers/productController');
 const { protect } = require('../middleware/authMiddleware');
 const commentRoutes = require('./commentRoutes');
 
 // Nested routes for comments
 router.use('/:id/comments', commentRoutes);
 
-// Products
-router.get('/', getProducts);
+// Create a new Product
 router.post('/', protect, createProduct);
+
+// Get all products
+router.get('/', getProducts);
+
+// Get a single product by ID
 router.get('/:id', getProductById);
+
+// Update a Product
+router.put('/:id', protect, updateProduct);
+
+// Delete a Product
+router.delete('/:id', protect, deleteProduct);
 
 module.exports = router;
