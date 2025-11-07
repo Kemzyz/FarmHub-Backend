179ecd420884229867ca4417d9b7089acb666cdc
Ehikwe Nkemkamma <ehikwenkemkamma@gmail.com>
2025-11-07 18:47:08 +0100

    Updated Version

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getProducts, getProductById, updateProduct, deleteProduct} = require('../controllers/productController');
+const { createProduct, getProducts, getProductById, updateProduct, deleteProduct } = require('../controllers/productController');
 const { protect } = require('../middleware/authMiddleware');
 const commentRoutes = require('./commentRoutes');
 
